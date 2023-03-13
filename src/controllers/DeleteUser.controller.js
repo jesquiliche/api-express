f93d18d4fabc33c59e106946a5ceb112d4df65ca
Jesús Quintana <jesquiliche@hotmail.comm>
2023-03-13 08:49:29 +0100

    Añadiendo más documentación

--- src/controllers/DeleteUser.controller.js
+++ src/controllers/DeleteUser.controller.js
@@ -1,12 +1,49 @@
+/**
+ * @swagger
+ * /api/user/{id}:
+ *  delete:
+ *    summary: Elimina un usuario existente de la base de datos
+ *    description: Elimina un usuario existente de la base de datos utilizando su ID.
+ *    tags:
+ *      - Users
+ *    parameters:
+ *      - in: header
+ *        name: auth-token
+ *        schema:
+ *          type: string
+ *        required: true
+ *        description: Token de autenticación válido.
+ *      - in: path
+ *        name: id
+ *        schema:
+ *          type: string
+ *        required: true
+ *        description: ID del usuario a eliminar.
+ *    responses:
+ *      200:
+ *        description: Usuario eliminado exitosamente
+ *      400:
+ *        description: Error en la petición o usuario no encontrado
+ *      500:
+ *        description: Error del servidor
+ */
+
+
+
+
 // Importar el middleware de validación de token y el modelo de usuario
 const verifyToken = require("../midleware/validate-token");
 const User=require("../models/User");
 
 // Función para borrar un usuario por su id
 const DeleteUser = async (req,res) => {
+    try{
     const {id}=req.params; // Obtener el id del usuario de los parámetros de la solicitud
     const user = await User.findByIdAndRemove(id) // Encontrar y eliminar el usuario
     res.status(204).json({message:"Borrado sadisfactoriamente"}) // Enviar una respuesta con un mensaje de éxito
+    }catch(error){
+        return res.status(500).json("Error interno del servidor")
+    }
 }
 
 // Exportar la función
--- src/controllers/UpdateUser.Controller.js
+++ src/controllers/UpdateUser.Controller.js
@@ -2,6 +2,69 @@ const User=require("../models/User");
 const encriptarContrasena=require("../midleware/encriptar-contrasena");
 const verifyToken = require("../midleware/validate-token");
 
+/**
+ * @swagger
+ * /api/user/{id}:
+ *  put:
+ *    summary: Actualiza un usuario existente en la base de datos
+ *    description: Actualiza un usuario existente en la base de datos utilizando su ID.
+ *    tags:
+ *      - Users
+ *    parameters:
+ *      - in: header
+ *        name: auth-token
+ *        schema:
+ *          type: string
+ *        required: true
+ *        description: Token de autenticación válido.
+ *      - in: path
+ *        name: id
+ *        schema:
+ *          type: string
+ *        required: true
+ *        description: ID del usuario a actualizar.
+ *    requestBody:
+ *      description: Información del usuario a actualizar
+ *      required: true
+ *      content:
+ *        application/json:
+ *          schema:
+ *            type: object
+ *            properties:
+ *              email:
+ *                type: string
+ *              nombre:
+ *                type: string
+ *              password:
+ *                type: string
+ *              apellidos:
+ *                type: string
+ *              telefono:
+ *                type: integer
+ *              direccion:
+ *                type: string
+ *              provincia:
+ *                type: string
+ *              poblacion:
+ *                type: string
+ *            example:
+ *              email: usuario@ejemplo.com
+ *              nombre: Nombre
+ *              password: 123456
+ *              apellidos: Apellidos
+ *              telefono: 123456789
+ *              direccion: Dirección
+ *              provincia: Provincia
+ *              poblacion: Población
+ *    responses:
+ *      200:
+ *        description: Usuario actualizado exitosamente
+ *      400:
+ *        description: Error en la petición o usuario no encontrado
+ *      500:
+ *        description: Error del servidor
+ */
+
 // Función para actualizar un usuario en la base de datos
 const UpdateUser = async (req,res) => {
 
